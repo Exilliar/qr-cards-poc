351e86181d472ab5b1dfe9ab42bbb0527845d1bd
exilliar <rorywebber1000@gmail.com>
2020-10-11 01:37:44 +0100

    #7 made basic header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <div class="d-flex align-center">
+      <span :style="textStyle">Card Collector</span>
+    </div>
+  </v-app-bar>
+</template>
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class Header extends Vue {
+  textStyle = {
+    fontSize: "2rem",
+    fontWeight: "400",
+  };
+}
+</script>
